6eeb06ac272ea114d44e28100db3548488d595da
Kysela Petr <petr@kysela.biz>
2016-03-30 01:43:47 +0200

    EDIT grunt tasks: doplněn komentář

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,10 @@
 module.exports = function(grunt) {
     'use strict';
 
+    require('time-grunt')(grunt);
+
+    // definice závislostí použitých v projektu,
+    // je blbost importovat celý bootstrap.min.js
     var devStackOptions = {
         vendorsJS: [
             'bower_components/jquery/dist/jquery.min.js',
@@ -22,6 +26,8 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        // spojení souborů pro méně http requestů
+        // rozděleno dle typu a pro větší rychlost tasků
         concat: {
             options: {
                 stripBanners: true,
@@ -44,6 +50,8 @@ module.exports = function(grunt) {
             }
         },
 
+        // uglifikace javascriptu
+        // rozděleno do subtasků kvůli rychlosti
         uglify: {
             options: {
                 report: 'gzip',
@@ -61,34 +69,37 @@ module.exports = function(grunt) {
             }
         },
 
+        // nebudeš prasit JavaScript a nebudeš!
         jshint: {
             main: ['www/src/js/*.js', 'www/src/js/**/*.js', 'www/src/js/**/**/*.js'],
             options: {
-                globals: {
-                    jQuery: true
-                },
+                jshintrc: '.jshintrc'
             }
         },
 
+        // kompilace stylů bootstrapu a samotné šablony webu
+        // -> rozděleno do samostatných subtasků kvůli rychlosti
         less: {
             bootstrap: {
                 files: {
-                    "www/dist/style/bootstrap.css": "www/src/style/bootstrap/main.less",
+                    'www/dist/style/bootstrap.css': 'www/src/style/bootstrap/main.less',
                 }
             },
             theme: {
                 files: {
-                    "www/dist/style/theme.css": "www/src/style/theme/main.less",
+                    'www/dist/style/theme.css': 'www/src/style/theme/main.less',
                 }
             }
         },
 
+        // fallback pro rem hodnoty a
+        // doplnění prefixů pro css dle caniuse.com
         postcss: {
             options: {
                 processors: [
                     require('pixrem')(),
                     require('autoprefixer')({
-                        browsers: ['> 4% in CZ', 'last 3 version']
+                        browsers: ['> 4% in CZ', 'last 3 version', 'ios 6', 'ie 7', 'ie 8', 'ie 9']
                     })
                 ]
             },
@@ -100,6 +111,8 @@ module.exports = function(grunt) {
             },
         },
 
+        // minifikace stylů bootstrapu a samotné šablony webu
+        // -> rozděleno do samostatných subtasků kvůli rychlosti
         cssmin: {
             bootstrap: {
                 files: {
@@ -113,6 +126,7 @@ module.exports = function(grunt) {
             }
         },
 
+        // zkopírování bower komponent do adresáře přístupného přes url
         copy: {
             lte: {
                 src: 'bower_components/lt-ie-9/lt-ie-9.min.js',
@@ -135,58 +149,37 @@ module.exports = function(grunt) {
             }
         },
 
-        // nahradit adresu k font awesome
+        // nahradit adresu k font awesome, jinak vyhazuje 404
         replace: {
             dist: {
                 options: {
-                  patterns: [
-                        {
-                          match: '../fonts/',
-                          replacement: '../../dist/font/'
-                        }
-                    ],
+                    patterns: [{
+                        match: '../fonts/',
+                        replacement: '../../dist/font/'
+                    }],
                     usePrefix: false
                 },
-                files: [
-                    {expand: false, flatten: true, src: 'www/dist/style/font-awesome.min.css', dest: 'www/dist/style/font-awesome.min.css'}
-                ]
-              }
+                files: [{
+                    expand: false,
+                    flatten: true,
+                    src: 'www/dist/style/font-awesome.min.css',
+                    dest: 'www/dist/style/font-awesome.min.css'
+                }]
+            }
         },
 
+        // mazání vygenerovaných souborů před novým generováním
         clean: {
-            bootstrap: 'www/dist/style/bootstrap.css',
-            theme: 'www/dist/style/theme.css',
+            bootstrap: ['www/dist/style/bootstrap.css', 'www/dist/style/main.css'],
+            theme: ['www/dist/style/theme.css', 'www/dist/style/main.css'],
             style: 'www/dist/style/*.css',
+            scriptApp: 'www/dist/js/main.js',
             script: 'www/dist/js/*.js'
         },
-        
-        watch: {
-            options: {
-                livereload: {
-                    host: 'localhost',
-                    port: 35729
-                },
-                interrupt: true,
-                debounceDelay: 1
-            },
-            style_bootstrap: {
-                files: 'www/src/style/bootstrap/*.less',
-                tasks: ['clean:bootstrap', 'less:bootstrap', 'concat:style']
-            },
-            style_theme: {
-                files: ['www/src/style/theme/*.less','www/src/style/theme/**/*.less'],
-                tasks: ['clean:theme', 'less:theme', 'concat:style']
-            },
-            script: {
-                files: ['www/src/js/*.js', 'www/src/js/**/*.js'],
-                tasks: ['clean:script','jshint', 'concat:application', 'concat:script']
-            },
-            template: {
-                files: ['www/_templates/*.html', 'www/_templates/**/*.html'],
-                tasks: []
-            },
-        },
 
+        // minifikace obrázků a grafiky webu,
+        // na png je ještě lepší tinypng.com
+        // ale lepší něco než-li ni
         image: {
             image_folder: {
                 options: {
@@ -207,6 +200,41 @@ module.exports = function(grunt) {
                     dest: 'www/dist/image/'
                 }]
             },
+        },
+
+        // watch task pro styly, js, šablony i gruntfile
+        watch: {
+            options: {
+                livereload: {
+                    host: 'localhost',
+                    port: 35729
+                },
+                interrupt: true,
+                debounceDelay: 1
+            },
+            style_bootstrap: {
+                files: 'www/src/style/bootstrap/*.less',
+                tasks: ['clean:bootstrap', 'less:bootstrap', 'concat:style']
+            },
+            style_theme: {
+                files: ['www/src/style/theme/*.less', 'www/src/style/theme/**/*.less'],
+                tasks: ['clean:theme', 'less:theme', 'concat:style']
+            },
+            script: {
+                files: ['www/src/js/*.js', 'www/src/js/**/*.js'],
+                tasks: ['clean:scriptApp', 'jshint', 'concat:application', 'concat:script']
+            },
+            template: {
+                files: ['www/_template/*.html', 'www/_template/**/*.html'],
+                tasks: []
+            },
+            image: {
+                files: 'www/src/image/*',
+                tasks: ['image']
+            },
+            grunt: {
+                files: ['Gruntfile.js']
+            }
         }
     });
 
@@ -214,6 +242,8 @@ module.exports = function(grunt) {
         concat: 'grunt-contrib-concat'
     });
 
+    // vývojové prostředí s livereloadem "grunt default"
     grunt.registerTask('default', ['clean', 'copy', 'replace', 'concat:vendors', 'jshint', 'concat:application', 'concat:script', 'image', 'less', 'concat:style', 'watch']);
+    // produkční prostředí -> vygenerování minifikovaných souborů pro commit "grunt dist"
     grunt.registerTask('dist', ['clean', 'copy', 'replace', 'concat:vendors', 'jshint', 'concat:application', 'concat:script', 'image', 'less', 'postcss', 'cssmin', 'concat:style', 'uglify']);
 };
